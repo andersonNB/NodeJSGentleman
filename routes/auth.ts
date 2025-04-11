d071ef627cc6b1da0b4643aa02ba9316fda42fd6
andersonNB <andersonaugustonb@ufps.edu.co>
2025-04-10 19:56:45 -0500

    FEATURE: authRoter - register o login

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,7 +1,15 @@
 import type {IncomingMessage, ServerResponse} from "http";
-import {authSchema, createUser, HttpMethod} from "../models";
+import {
+	authSchema,
+	createUser,
+	findUserByEmail,
+	HttpMethod,
+	validatePassword,
+} from "../models";
 import {parseBody} from "../utils/parseBody";
 import {safeParse} from "valibot";
+import {sign} from "jsonwebtoken";
+import config from "../config";
 
 export const authRouter = async (req: IncomingMessage, res: ServerResponse) => {
 	const {method, url} = req;
@@ -30,5 +38,49 @@ export const authRouter = async (req: IncomingMessage, res: ServerResponse) => {
 				res.end("Internal server error");
 			}
 		}
+	}
+
+	if (url === "/auth/login" && method === HttpMethod.POST) {
+		const body = await parseBody(req);
+		const result = safeParse(authSchema, body);
+
+		if (result.issues) {
+			res.statusCode = 400;
+			res.end("Invalid request body");
+			return;
+		}
+
+		const {email, password} = body;
+
+		const user = findUserByEmail(email);
+
+		if (!user || !(await validatePassword(user, password))) {
+			res.statusCode = 401;
+			res.end("Invalid email or password");
+			return;
+		}
+
+		const accessToken = sign(
+			{id: user.id, email: user.email, role: user.role},
+			config?.jwtSecret,
+			{expiresIn: "1h"}
+		);
+
+		const refreshToken = sign(
+			{id: user.id, email: user.email, role: user.role},
+			config?.jwtSecret,
+			{expiresIn: "1d"}
+		);
+
+		user.refreshToken = refreshToken;
+
+		res.end(
+			JSON.stringify({
+				accessToken,
+				refreshToken,
+			})
+		);
+
+		return;
 	}
 };
